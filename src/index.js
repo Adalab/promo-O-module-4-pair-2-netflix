66d0bfc4ea11384e86faf1c86a5631b3cb16322a
ElenaFernandez <elena.fernandez92@gmail.com>
2022-02-04 11:22:42 +0100

    add exercises 4.4

--- src/index.js
+++ src/index.js
@@ -1,44 +1,56 @@
-const express = require("express");
-const cors = require("cors");
-const movies = require("./data/movies.json");
+const express = require('express');
+const cors = require('cors');
+const movies = require('./data/movies.json');
+
 // create and config server
 const server = express();
 server.use(cors());
 server.use(express.json());
+server.set('view engine', 'ejs');
 
 // init express aplication
 const serverPort = 4000;
 server.listen(serverPort, () => {
-	console.log(`Server listening at http://localhost:${serverPort}`);
+  console.log(`Server listening at http://localhost:${serverPort}`);
 });
 
-server.get("/movies", (req, res) => {
-	console.log(req.query);
-	const genderFilterParam = movies.filter(
-		(movie) => movie.gender === req.query.gender
-	);
-	res.send({
-		success: true,
-		movies: genderFilterParam,
-	});
+server.get('/movie/:movieId', (req, res) => {
+  console.log(req.params.movieId);
+  const foundMovie= movies.find((movie)=> movie.id === req.params.movieId)
+  console.log(foundMovie)
+  res.render('movie', foundMovie);
 });
 
-server.post("/login", (req, res) => {
-	if (req.body.email.includes("gmail")) {
-		res.json({
-			success: true,
-			userId: "123",
-		});
-	} else {
-		res.json({
-			success: false,
-			errorMessage: "Usuario no encontrado",
-		});
-	}
+server.get('/movies', (req, res) => {
+  //console.log(req.query);
+  const genderFilterParam = movies.filter(
+    (movie) => movie.gender === req.query.gender
+  );
+  res.send({
+    success: true,
+    movies: genderFilterParam,
+  });
 });
 
-const staticServerPath = "./src/public-react";
+server.post('/login', (req, res) => {
+  if (req.body.email.includes('gmail')) {
+    res.json({
+      success: true,
+      userId: '123',
+    });
+  } else {
+    res.json({
+      success: false,
+      errorMessage: 'Usuario no encontrado',
+    });
+  }
+});
+
+const staticServerPath = './src/public-react';
 server.use(express.static(staticServerPath));
 
-const staticServerPathImages = "./src/public-movies-images";
+const staticServerPathImages = './src/public-movies-images';
 server.use(express.static(staticServerPathImages));
+
+const staticServerPathCss ='//beta.adalab.es/resources/stylesheets/all.css';
+server.use(express.static(staticServerPathCss));
